a307adf613a71f11786b1e415bff2e0b8ecff1d2
noel nyakundi <noel.nyakundi@student.moringaschool.com>
2024-03-30 15:42:30 +0300

    map

--- index.js
+++ index.js
@@ -11,6 +11,26 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+
 const titleCased = () => {
-  return tutorials
+  
+  const firstCap = (string) => {
+    return string.replace(/\b\w/g, function (char) {
+      return char.toUpperCase();
+    });
+  };
+
+  
+  return tutorials.map(function (tutorials) {
+    
+    let newtut = tutorials.split(' ');
+    
+    for (let i = 0; i < newtut.length; i++) {
+      newtut[i] = firstCap(newtut[i]);
+    }
+    
+    return newtut.join(' ');
+  });
 }
+
+
